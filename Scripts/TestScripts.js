2985915a3d64684979ad9603ed7817a6711f0345
David Rath <adverserath@gmail.com>
2020-11-12 22:19:44 +0000

    HelperTest

--- Scripts/TestScripts.js
+++ Scripts/TestScripts.js
@@ -1,28 +1,26 @@
-function GetTargetAndPrint()
-{
- TextWindow.Print('not : ' + (3&15));
-  Orion.WaitForAddObject('myTarget');
-  Orion.TargetObject('myTarget');
-  var obj = Orion.FindObject('myTarget');
-          TextWindow.Open();
-        //  Orion.GetProfile('myTarget',2000)
-    TextWindow.Print(obj.Name());
-        TextWindow.Print(obj.FullName());
-                TextWindow.Print(obj.Notoriety());
+//#include helpers/Helper.js
+function GetTargetAndPrint() {
+  TextWindow.Print('not : ' + (3 & 15));
+  var obj = SelectTarget();
+  TextWindow.Open();
+  //  Orion.GetProfile('myTarget',2000)
+  TextWindow.Print(obj.Name());
+  TextWindow.Print(obj.FullName());
+  TextWindow.Print(obj.Notoriety());
 
-    TextWindow.Print(obj.Race());
-    TextWindow.Print(obj.ProfileReceived());
-    TextWindow.Print(obj.Flags());
-        TextWindow.Print(obj.IgnoreCharacters());
-        TextWindow.Print(obj.NameColor());
-        TextWindow.Print((''+obj.Title()).substr(0,3) );
+  TextWindow.Print(obj.Race());
+  TextWindow.Print(obj.ProfileReceived());
+  TextWindow.Print(obj.Flags());
+  TextWindow.Print(obj.IgnoreCharacters());
+  TextWindow.Print(obj.NameColor());
+  TextWindow.Print(('' + obj.Title()).substr(0, 3));
 }
 
 function getFlags() {
   Orion.WaitForAddObject('myTarget');
   Orion.TargetObject('myTarget');
   var target = Orion.FindObject('myTarget');
- TextWindow.Print(Orion.B);
+  TextWindow.Print(Orion.B);
 
 }
 
@@ -30,36 +28,33 @@ function getTileData() {
   Orion.WaitForAddObject('myTarget');
   Orion.TargetObject('myTarget');
   var target = Orion.FindObject('myTarget');
-  
+
   var tiles = Orion.GetTiles('land', Player.X(), Player.Y(), 0, 100);
   Orion.Print('tile' + tiles.length);
   Orion.Print('tile' + tiles[0].Z());
 }
 
-function test(){
+function test() {
 
-TextWindow.Open();
-while(!Player.Dead())
-{
+  TextWindow.Open();
+  while (!Player.Dead()) {
 
-Orion.InfoContextMenu();
+    Orion.InfoContextMenu();
 
 
-Orion.Wait(500);
-}
-}
-
-function getPeople()
-{
-Orion.FindTypeEx(any, any, ground, 'human|live|ignoreself|ignorefriends', 20, 7)
-.foreach(function (mob){
-TextWindow.Print(mob.Name());
-});
+    Orion.Wait(500);
+  }
 }
 
-function sell()
-{
-Orion.Buy('bInscribe');
+function getPeople() {
+  Orion.FindTypeEx(any, any, ground, 'human|live|ignoreself|ignorefriends', 20, 7)
+    .foreach(function (mob) {
+      TextWindow.Print(mob.Name());
+    });
+}
+
+function sell() {
+  Orion.Buy('bInscribe');
 }
 //    Orion.WaitForAddObject('myTarget');
 //    var enemy = Orion.TargetObject('myTarget');
