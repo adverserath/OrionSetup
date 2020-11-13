09acd0c36ab410649cc7cebc731e73e121aba107
David Rath <adverserath@gmail.com>
2020-11-13 10:18:40 +0000

    lumberjack scritps

--- Scripts/TestScripts.js
+++ Scripts/TestScripts.js
@@ -1,4 +1,49 @@
 //#include helpers/Helper.js
+
+function TestLumber() {
+  Orion.Wait(1000);
+  var n = 'There\'s not enough wood here to harvest.'.localeCompare('There\'s not enough wood here to harvest.'==0);
+  var range = 20;
+  var trees = '0x0CCD|0x0CDD|0x0CD3'
+  Orion.GetTilesInRect(trees, Player.X() - range, Player.Y() - range, Player.X() + range, Player.Y() + range).
+    forEach(function (treeTile) {
+
+      //Orion.WalkTo(x, y, z, distanceXY, distanceZ, run, openDoor, maxWalkingTime);
+      //Orion.WalkTo(treeTile.X, treeTile.Y(), treeTile.Z(), 1, 255, 1, 0);
+      Orion.WalkTo(treeTile.X(), treeTile.Y(), treeTile.Z(), 1, Player.Z(), 1, 1);
+      TextWindow.Print('Walking');
+	Chop(treeTile.Graphic(),treeTile.X(),treeTile.Y().treeTile,Player.Z());
+
+    });
+}
+
+function Chop(id, x, y, z) {
+while('There\'s not enough wood here to harvest.'.localeCompare(Orion.LastJournalMessage().Text())!=0)
+{
+TextWindow.Print('There\'s not enough wood here to harvest.')
+
+TextWindow.Print(Orion.LastJournalMessage().Text())
+
+TextWindow.Print('There\'s not enough wood here to harvest.'.localeCompare(Orion.LastJournalMessage()))
+  Orion.GetTilesInRect(any, Player.X() + 1, Player.Y() + 1, Player.X() - 1, Player.Y() - 1)
+    .forEach(function (tile) {
+TextWindow.Print(tile.Graphic());
+
+      Orion.UseObject('0x462D1383');
+      Orion.Wait(300);
+
+      if (Orion.WaitForTarget(1000)) {
+        TextWindow.Print(tile.Flags());
+        Orion.TargetTile(any, tile.X(), tile.Y(), tile.Z());
+        Orion.Wait(3000);
+      }
+
+
+    });
+}
+  //0x0CCD 3492 2718 6
+}
+
 function GetTargetAndPrint() {
   TextWindow.Print('not : ' + (3 & 15));
   var obj = SelectTarget();
@@ -30,8 +75,8 @@ function getTileData() {
   var target = Orion.FindObject('myTarget');
 
   var tiles = Orion.GetTiles('land', Player.X(), Player.Y(), 0, 100);
-  Orion.Print('tile' + tiles.length);
-  Orion.Print('tile' + tiles[0].Z());
+  TextWindow.Print('tile' + tiles.length);
+  TextWindow.Print('tile' + tiles[0].Z());
 }
 
 function test() {
