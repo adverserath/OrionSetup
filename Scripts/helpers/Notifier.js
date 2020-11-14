dd3445506a6765e14dbc1d90ed282d472444ee38
David Rath <adverserath@gmail.com>
2020-11-14 14:27:37 +0000

    Add notifier script

---
+++ Scripts/helpers/Notifier.js
@@ -0,0 +1,29 @@
+var hook;
+var key;
+function callBot()
+{
+BotPush("hey");
+}
+//Create discord.conf in "Orion Launcher" folder
+//First Word 1  = Discord hook ID
+//single space
+//Second Word 2 = API key
+function BotPush(message) {
+    if (hook == null && key == null) {
+    var file  =   Orion.NewFile();
+        open = file.Open('discordkey.conf',true);
+        if (!file.Opened()) {
+            return;
+        }
+        hook = file.Read();
+        key = file.Read();
+        file.Close();
+    }
+    var bot = 
+    "https://discordapp.com/api/webhooks/"
+    +hook
+    +"/"
+    +key; // Webhook url
+    var paramText = "content="+message;
+    Orion.HttpPost(bot, paramText);
+}
