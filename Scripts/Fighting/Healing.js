b54cd648c6320d7446f40cad112e6d438e226682
David Rath <adverserath@gmail.com>
2020-11-20 10:20:53 +0000

    -show bandage timer

--- Scripts/Fighting/Healing.js
+++ Scripts/Fighting/Healing.js
@@ -1,14 +1,14 @@
-function Bandage() {
+function BandageSelf() {
     while (!Player.Dead()) {
-        Orion.Wait(200);
-        if (Player.Hits() < Player.MaxHits()) {
+        if (!Orion.BuffExists('healing skill') && (Player.Hits() < Player.MaxHits() || Player.Poisoned())) {
+
             Orion.BandageSelf();
             Orion.Wait(100);
-            Orion.AddDisplayTimer('bandageSelf', Orion.BuffTimeRemaining('healing skill'));
-            while (Orion.BuffExists('healing skill')) {
-                Orion.Wait(100);
-            }
+        }
+        ShowTimerSelf();
 
+        while (Orion.BuffExists('healing skill')) {
+            Orion.Wait(100);
         }
     }
 }
@@ -21,20 +21,24 @@ function HealingSelfAndFriendLoop() {
     while (Player.IsHuman) {
         Orion.Wait(200);
         if (target != null) {
-            if (target != null && target.Hits() < (target.MaxHits() - 10)
+            if (target != null && (target.Poisoned() || target.Hits() < (target.MaxHits() - 5))
                 && !Orion.BuffExists('healing skill') && target.Distance() <= 2
                 && ((Player.Hits() / Player.MaxHits()) > (target.Hits() / target.MaxHits()))) {
                 Orion.BandageTarget('myTarget');
                 Orion.AddDisplayTimer('bandageFriend',
                     Orion.BuffTimeRemaining('healing skill'),
                     'Top', 'Circle', 'HealOther', 0, 0,
-                    'any', -1, '0x0000FFFE'); Orion.Wait(500);
+                    'any', -1, '0x0000FFFE');
+                Orion.DisplayTimerSetIcon('bandageFriend', 'Top', '0x0E21');
+                Orion.Wait(500);
             }
-            if (!Orion.BuffExists('healing skill') && Player.Hits() < Player.MaxHits() && (Player.Hits() / Player.MaxHits() < target.Hits() / target.MaxHits() || target.Distance() >= 2)) {
+            if (!Orion.BuffExists('healing skill') && (Player.Poisoned() || Player.Hits() < Player.MaxHits()) && (Player.Hits() / Player.MaxHits() < target.Hits() / target.MaxHits() || target.Distance() >= 2)) {
                 Orion.AddDisplayTimer('bandageSelf',
                     Orion.BuffTimeRemaining('healing skill'),
                     'Top', 'Circle', 'HealSelf', 0, 0,
                     'any', -1, '0x00FF00FE'); Orion.BandageSelf();
+                Orion.DisplayTimerSetIcon('bandageSelf', 'Center', '0x0E21');
+
                 Orion.Wait(500);
             }
         }
@@ -48,6 +52,9 @@ function HealingSelfAndFriendLoop() {
                     Orion.BuffTimeRemaining('healing skill'),
                     'Top', 'Circle', 'HealSelf', 0, 0,
                     'any', -1, '0x00FF00FE');
+                Orion.DisplayTimerSetIcon('bandageSelf', 'Top', '0x0E21');
+
+
                 while (Orion.BuffExists('healing skill')) {
                     Orion.Wait(100);
                 }
@@ -55,4 +62,19 @@ function HealingSelfAndFriendLoop() {
         }
     }
 
+}
+
+function ShowTimerSelf() {
+    Orion.AddDisplayTimer('bandageSelf',
+        Orion.BuffTimeRemaining('healing skill'),
+        'AboveChar', 'Circle|Bar', 'HealSelf', -50, 0,
+        '0xFFFF00FE', -1, '0xFFFF00FE');
+    Orion.DisplayTimerSetIcon('bandageSelf', 'Top', '0x0E21', '00FFFF');
+}
+function ShowTimerOther() {
+    Orion.AddDisplayTimer('bandageSelf',
+        Orion.BuffTimeRemaining('healing skill'),
+        'AboveChar', 'Circle|Bar', 'HealSelf', -50, 0,
+        '0x00FF00FE', -1, '0x00FF00FE');
+    Orion.DisplayTimerSetIcon('bandageSelf', 'Top', '0x0E21', '00FFFF');
 }
