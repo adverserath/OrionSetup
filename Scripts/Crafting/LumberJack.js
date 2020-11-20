56a35211f834e2c80be83df3c9683e1d99216503
David Rath <adverserath@gmail.com>
2020-11-20 09:35:04 +0000

    -started working on all craft trainer -healing timers

--- Scripts/Crafting/LumberJack.js
+++ Scripts/Crafting/LumberJack.js
@@ -3,6 +3,23 @@
 //#include helpers/Debug.js
 //#include helpers/ItemManager.js
 
+//Cut trees within a defined range of Player.
+//Once all trees are cut in the grid, the script will terminate!
+//THIS IS NOT INTENDED FOR AFK USAGE
+//On Start - select storage and runes to recall
+//Press Escape to use values from the last run
+//Go into war mode to pause the script if you need to fight anything
+//All axes are detected automatically.
+function StartLumberJacking() {
+    //Use Mark and Recall spells to move to storage and back to last location
+    //Otherwise it will use Orions WalkTo functionality
+    var useMagicToMove = true;
+
+    //How far to look for trees from the player
+    var range = 20;
+    AutoLumberJack(useMagicToMove, range);
+}
+
 
 var debug = true;
 var axes = '0xF47|0xF4B|0xF45|0xF43|0x13FB|0x1443|0x13B0|0xF49'
--- Scripts/Fighting/Healing.js
+++ Scripts/Fighting/Healing.js
@@ -21,29 +21,38 @@ function HealingSelfAndFriendLoop() {
     while (Player.IsHuman) {
         Orion.Wait(200);
         if (target != null) {
-            if (target != null && target.Hits() < target.MaxHits()
+            if (target != null && target.Hits() < (target.MaxHits() - 10)
                 && !Orion.BuffExists('healing skill') && target.Distance() <= 2
-                && (Player.Hits() / Player.MaxHits() > target.Hits() / target.MaxHits())) {
+                && ((Player.Hits() / Player.MaxHits()) > (target.Hits() / target.MaxHits()))) {
                 Orion.BandageTarget('myTarget');
-                Orion.AddDisplayTimer('bandageSelf', Orion.BuffTimeRemaining('healing skill'));
+                Orion.AddDisplayTimer('bandageFriend',
+                    Orion.BuffTimeRemaining('healing skill'),
+                    'Top', 'Circle', 'HealOther', 0, 0,
+                    'any', -1, '0x0000FFFE'); Orion.Wait(500);
+            }
+            if (!Orion.BuffExists('healing skill') && Player.Hits() < Player.MaxHits() && (Player.Hits() / Player.MaxHits() < target.Hits() / target.MaxHits() || target.Distance() >= 2)) {
+                Orion.AddDisplayTimer('bandageSelf',
+                    Orion.BuffTimeRemaining('healing skill'),
+                    'Top', 'Circle', 'HealSelf', 0, 0,
+                    'any', -1, '0x00FF00FE'); Orion.BandageSelf();
                 Orion.Wait(500);
             }
-            if (Player.Hits() < Player.MaxHits() && (Player.Hits() / Player.MaxHits() < target.Hits() / target.MaxHits() || target.Distance() >= 2)) {
+        }
+        else {
+            Orion.Wait(200);
+            if (Player.Hits() < Player.MaxHits()) {
                 Orion.BandageSelf();
-                Orion.Wait(500);
-            }
-        }
-        else{
-        Orion.Wait(200);
-        if (Player.Hits() < Player.MaxHits()) {
-            Orion.BandageSelf();
-            Orion.Wait(100);
-            Orion.AddDisplayTimer('bandageSelf', Orion.BuffTimeRemaining('healing skill'));
-            while (Orion.BuffExists('healing skill')) {
                 Orion.Wait(100);
+
+                Orion.AddDisplayTimer('bandageSelf',
+                    Orion.BuffTimeRemaining('healing skill'),
+                    'Top', 'Circle', 'HealSelf', 0, 0,
+                    'any', -1, '0x00FF00FE');
+                while (Orion.BuffExists('healing skill')) {
+                    Orion.Wait(100);
+                }
             }
-        }
         }
     }
-   
+
 }
