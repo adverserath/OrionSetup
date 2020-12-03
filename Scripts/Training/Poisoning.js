7f7c58537ed563d9c07525acf254ef68cc7e06e2
David Rath <adverserath@gmail.com>
2020-12-03 16:40:06 +0000

    -poison move bottles

--- Scripts/Training/Poisoning.js
+++ Scripts/Training/Poisoning.js
@@ -1,4 +1,6 @@
 //#include Scripts/helpers/Notifier.js
+//#include Scripts/helpers/ItemManager.js
+//#include Scripts/helpers/Debug.js
 
 function TrainPoison()
 {
@@ -13,6 +15,10 @@ Orion.UseType('0x0F07', '0xFFFF');
 Orion.TargetType('0x0F0A', '0xFFFF','0x46415E83');
 	if (Orion.WaitForTarget(1000))
 		Orion.TargetObject('0x43F6481D');
+if(Player.Weight()>Player.MaxWeight())
+{
+MoveItemsFromPlayer(Orion.FindObject('0x46415E83'), '0x0F0E')
+}
 }
 Orion.Wait(1000);
 BotPush("You are dead");
