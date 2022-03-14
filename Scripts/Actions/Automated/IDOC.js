39d46b878a6e0fb2f6fe87908382e0935cac8449
David Rath <adverserath@gmail.com>
2022-03-14 13:55:25 +0000

    House checker

--- Scripts/Actions/Automated/IDOC.js
+++ Scripts/Actions/Automated/IDOC.js
@@ -3,6 +3,7 @@
 //#include helpers/Beetle.js
 //#include helpers/ItemManager.js
 
+var maps = ['Felucca', 'Trammel', 'Ilshenar', 'Malas', 'Tokuno']
 function IDOCAlert() {
     var selected;
     var selecting = true;
@@ -72,8 +73,17 @@ function IDOCWalker() {
 
 }
 function IDOCScanner() {
+    Orion.IgnoreReset()
     while (true) {
         Orion.Wait(500)
+        Orion.FindTypeEx(any, any, ground, 'item', 50).filter(function (item) {
+            return item.Name() === 'A House Sign'
+        })
+            .forEach(function (sign) {
+                var condition = sign.Properties().match(/Condition.*/gi)
+                Orion.PrintFast(sign.Serial(), 68, 1, condition);
+
+            })
         var houseSigns = Orion.FindTypeEx(any, any, ground, 'item', 50).filter(function (item) {
             return item.Name() === 'A House Sign'
                 && (item.Properties().match(/Condition..This\sstructure\sis\sin\sdanger\sof\scollapsing/gi)
@@ -82,12 +92,80 @@ function IDOCScanner() {
         })
         houseSigns.forEach(function (houseSign) {
             WalkTo(houseSign)
-            BotPush('X: ' + houseSign.X() + ' Y: ' + houseSign.Y())
+            BotPush('Map: ' + maps[Player.Map()] + '  X: ' + houseSign.X() + ' Y: ' + houseSign.Y())
             BotPush(houseSign.Properties())
             Orion.Print(houseSign.Properties())
 
             Orion.Wait(5000)
             Orion.Ignore(houseSign.Serial())
         })
+    }
+}
+
+
+function GrubberTracking() {
+    TextWindow.Open()
+    var count = 0
+    var found = 0
+    while (!Player.Dead()) {
+        var Gump;
+        Orion.UseSkill("Tracking");
+        Orion.Wait(500);
+        Gump = Orion.GetLastGump();
+        Gump.Select(Orion.CreateGumpHook(1));
+        Orion.Wait(1000);
+        Gump = Orion.GetLastGump();
+        count++
+        if (Gump.TextList().indexOf('grubber') != -1) {
+            Orion.Print(39, 'found grubber')
+
+        }
+        Orion.Wait(3000);
+
+        Gump.Close()
+        Orion.Wait(6000);
+    }
+    Orion.Wait(1000);
+}
+
+function ReadHouseAccessor() {
+    var HouseAccessor = Orion.FindTypeEx('0x22C4', any, ground, 'item|near', 2)[0]
+    var start = Orion.Now()
+    Orion.UseObject(HouseAccessor.Serial())
+    Orion.Wait(100)
+    var message = Orion.WaitJournal('', start, Orion.Now() + 1000, 'item', HouseAccessor.Serial());
+    if (message != null) {
+        var hm = message.Text()
+        var values = hm.match(/\d+/g)
+        var houses = parseInt(values[0])
+        var idoc = parseInt(values[1])
+        var greatly = parseInt(values[2])
+        Orion.Wait(1000)
+        while (HouseAccessor != null && HouseAccessor.Distance() < 3) {
+            start = Orion.Now()
+            Orion.UseObject(HouseAccessor.Serial())
+            var message = Orion.WaitJournal('', start, Orion.Now() + 1000, 'item', HouseAccessor.Serial());
+            if (message != null) {
+                var hm = message.Text()
+                var values = hm.match(/\d+/g)
+
+                if (parseInt(values[0]) != houses) {
+                    BotPush('Houses ' + (parseInt(values[0]) - houses))
+                }
+                var houses = parseInt(values[0])
+
+                if (parseInt(values[1]) != idoc) {
+                    BotPush('IDOC ' + (parseInt(values[1]) - idoc))
+                }
+                var idoc = parseInt(values[1])
+
+                if (parseInt(values[2]) != greatly) {
+                    BotPush('Greatly ' + (parseInt(values[2]) - greatly))
+                }
+                var greatly = parseInt(values[2])
+
+            }
+            Orion.Wait(10000)
+        }
     }
 }
